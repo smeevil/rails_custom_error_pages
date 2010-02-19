116b026bb4af7ed13036ddd4ce7eb60d905c2701
Wes Oldenbeuving <narnach@gmail.com>
2010-02-19 10:50:44 +0100

    Clarified instructions, fixed installer to check for haml being present

--- install.rb
+++ install.rb
@@ -1,18 +1,31 @@
-if defined?(Haml)
-  puts "Copying files..."
-  path=File.expand_path(File.dirname(__FILE__))
-  ["app", "public"].each do |dir|
-    FileUtils.cp_r("#{path}/#{dir}", RAILS_ROOT)
-  end
-  puts "Files copied - Installation complete!"
+begin
+  require 'haml'
+rescue LoadError
+  puts <<-INSTRUCTIONS
+This plugin require HAML, please install it:
+  sudo gem install haml
 
-  puts "If you want to generate custom 404 pages then edit routes.rb and remove the default routes :"
-  puts "map.connect ':controller/:action/:id'"
-  puts "map.connect ':controller/:action/:id.:format'"
-  puts
-  puts "Then add this line AT THE BOTTOM of your routes.rb :"
-  puts "map.not_found '*path', :controller => 'application', :action => 'render_not_found'"
-else
-  puts "This plugin require HAML , please install it : sudo gem install haml"
-  puts "Then retry this install again..."
+Then add it to your environment.rb file:
+  # config/environment.rb:
+  config.gem 'haml'
+INSTRUCTIONS
 end
+
+puts "Copying files..."
+path=File.expand_path(File.dirname(__FILE__))
+["app", "public"].each do |dir|
+  FileUtils.cp_r("#{path}/#{dir}", RAILS_ROOT)
+end
+
+puts <<-INSTRUCTIONS
+Files copied - Installation complete!
+
+If you want to generate custom 404 pages then edit your routes file and remove the default routes :
+  # config/routes.rb
+  map.connect ':controller/:action/:id'"
+  map.connect ':controller/:action/:id.:format'
+
+Then add this line AT THE BOTTOM of your routes file:
+  # config/routes.rb
+  map.not_found '*path', :controller => 'application', :action => 'render_not_found'
+INSTRUCTIONS
