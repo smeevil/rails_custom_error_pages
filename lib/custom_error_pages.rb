4e7d3bbe72d48e9e1d83279831d6dc7a1b71a468
Wes Oldenbeuving <narnach@gmail.com>
2011-12-19 14:17:39 +0100

    Rails 3.1.x changes + Hoptoad is now Airbrake

--- lib/custom_error_pages.rb
+++ lib/custom_error_pages.rb
@@ -16,7 +16,7 @@ module CustomErrorPages
         logger.debug "Exception matched. Logging."
         log_error(exception)
         logger.debug "Notifying hoptoad"
-        attempt_to_notify_hoptoad(exception)
+        attempt_to_notify_airbrake(exception)
         if respond_to?(:activate_authlogic)
           logger.debug "Activating authlogic"
           activate_authlogic
@@ -25,9 +25,11 @@ module CustomErrorPages
         logger.debug "Not an exception, treating it as custom message."
         @message = exception
       end
-      respond_to do |format|
-        format.html {render :template => "/application/500", :status => 500, :layout=>"custom_error_page"}
-        format.xml {render :template => "/application/500", :status => 500}
+      unless performed?
+        respond_to do |format|
+          format.html { render :template => "/application/500", :status => 500, :layout=>"custom_error_page" }
+          format.xml { render :template => "/application/500", :status => 500 }
+        end
       end
     end
 
@@ -37,18 +39,20 @@ module CustomErrorPages
       if current_user
         flash[:error] = "Access denied. You have insufficient privileges to go here."
       else
-        session[:stored_location] = request.request_uri
+        session[:stored_location] = request.url
         flash[:error] = "Access denied. Try to log in first."
       end
-      respond_to do |format|
-        format.html do
-          if current_user
-            render :template => 'application/403', :layout=>"custom_error_page" , :status=>403
-          else
-            redirect_to login_path
+      unless performed?
+        respond_to do |format|
+          format.html do
+            if current_user
+              render :template => 'application/403', :layout=>"custom_error_page", :status=>403
+            else
+              redirect_to(login_path)
+            end
           end
+          format.xml { render :template => 'application/403', :status=>403 }
         end
-        format.xml {render :template => 'application/403', :status=>403}
       end
     end
 
@@ -56,15 +60,17 @@ module CustomErrorPages
       logger.debug "Not found"
       if exception.kind_of? Exception
         log_error(exception)
-        attempt_to_notify_hoptoad(exception)
+        attempt_to_notify_airbrake(exception)
       elsif exception.nil? && params[:path].is_a?(Array)
         # Catch-all route defined in routes.rb. Just render the 404.
       else
         @message = exception
       end
-      respond_to do |format|
-        format.html {render :template => "/application/404", :status => 404, :layout=>"custom_error_page"}
-        format.xml {render :template => "/application/404", :status => 404}
+      unless performed?
+        respond_to do |format|
+          format.html { render :template => "/application/404", :status => 404, :layout=>"custom_error_page" }
+          format.xml { render :template => "/application/404", :status => 404 }
+        end
       end
     end
 
@@ -85,19 +91,19 @@ module CustomErrorPages
       end
     rescue Exception => e
       logger.info "Error while handling errors: #{e.inspect}. Notifying hoptoad."
-      attempt_to_notify_hoptoad(e)
+      attempt_to_notify_airbrake(e)
       raise e
     end
 
-    def attempt_to_notify_hoptoad(exception)
-      if defined?(HoptoadNotifier::Rails::ControllerMethods) and self.class.included_modules.include?(HoptoadNotifier::Rails::ControllerMethods)
-        logger.debug "Notify hoptoad by means of notify_hoptoad"
-        notify_hoptoad(exception)
-      elsif defined?(HoptoadNotifier) and HoptoadNotifier.respond_to?(:notify)
-        logger.debug "Notify hoptoad by means of HoptoadNotifier.notify"
-        HoptoadNotifier.notify(exception)
+    def attempt_to_notify_airbrake(exception)
+      if defined?(Airbrake::Rails::ControllerMethods) and self.class.included_modules.include?(Airbrake::Rails::ControllerMethods)
+        logger.debug "Notify airbrake by means of notify_airbrake"
+        notify_airbrake(exception)
+      elsif defined?(Airbrake) and Airbrake.respond_to?(:notify)
+        logger.debug "Notify airbrake by means of Airbrake.notify"
+        Airbrake.notify(exception)
       else
-        logger.debug "We have no way to notify Hoptoad. Is the hoptoad_notifier gem even installed?"
+        logger.debug "We have no way to notify Airbrake. Is the airbrake_notifier gem even installed?"
       end
     end
 
@@ -117,16 +123,16 @@ module CustomErrorPages
 
       def not_found_errors
         errors = []
-        errors << ActiveRecord::RecordNotFound if defined?(ActiveRecord::RecordNotFound) and not ActionController::Base.consider_all_requests_local
-        errors << ActionController::RoutingError if defined?(ActionController::RoutingError) and not ActionController::Base.consider_all_requests_local
-        errors << ActionController::UnknownController if defined?(ActionController::UnknownController) and not ActionController::Base.consider_all_requests_local
-        errors << ActionController::UnknownAction if defined?(ActionController::UnknownAction) and not ActionController::Base.consider_all_requests_local
+        errors << ActiveRecord::RecordNotFound if defined?(ActiveRecord::RecordNotFound) and not Rails.application.config.consider_all_requests_local
+        errors << ActionController::RoutingError if defined?(ActionController::RoutingError) and not Rails.application.config.consider_all_requests_local
+        errors << ActionController::UnknownController if defined?(ActionController::UnknownController) and not Rails.application.config.consider_all_requests_local
+        errors << ActionController::UnknownAction if defined?(ActionController::UnknownAction) and not Rails.application.config.consider_all_requests_local
         errors
       end
 
       def real_errors
         errors = []
-        errors << Exception if not ActionController::Base.consider_all_requests_local
+        errors << Exception if not Rails.application.config.consider_all_requests_local
         errors
       end
     end
@@ -138,7 +144,7 @@ module CustomErrorPages
       files=Dir.glob("#{RAILS_ROOT}/public/images/#{err.to_s}/*")
       # Rails 2.3.6 deprecates the old Array#rand in favor of Array#random_element.
       random_method = files.respond_to?(:random_element) ? :random_element : :rand
-      files.send(random_method).gsub("#{RAILS_ROOT}/public/images/","")
+      files.send(random_method).gsub("#{RAILS_ROOT}/public/images/", "")
     end
   end
 end
